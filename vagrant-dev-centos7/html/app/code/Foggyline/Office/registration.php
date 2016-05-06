51a5626191bbcfd70f4b28d0c857ab752c55138b
hein168 <vitali.heinrich@bertelsmann.de>
2016-05-06 09:14:26 +0200

    removed foggyline office

--- vagrant-dev-centos7/html/app/code/Foggyline/Office/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: hein168
- * Date: 28.04.2016
- * Time: 10:29
- */
-\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE, 'Foggyline_Office', __DIR__);
